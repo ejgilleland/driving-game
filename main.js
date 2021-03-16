3ca5f788eadf44fffc2c115c38389d9948f15acb
Jared Gilleland <ejgilleland@gmail.com>
2021-03-15 23:27:03 -0700

    Added car's location data to car object (positionX & positionY properties), listened for spacebar keydown, added setInterval to update car's left CSS property via car's positionX property. Issue complete #3

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 
 var car = {
   $car: document.querySelector('img'),
-  direction: 'east'
+  direction: 'east',
+  moving: false,
+  positionX: 0,
+  positionY: 0
 };
 
 window.addEventListener('keydown', function (event) {
-  if (event.key === 'ArrowRight') {
+  if (event.key === ' ') {
+    car.moving = !car.moving;
+    window.setInterval(function () {
+      car.positionX += 8;
+      car.$car.style.left = car.positionX + 'px';
+    }, 16);
+  } else if (event.key === 'ArrowRight') {
     car.direction = 'east';
   } else if (event.key === 'ArrowDown') {
     car.direction = 'south';
