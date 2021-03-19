a8fe9121d82b2277daf55fadbc7eb5a8e8fc86c8
Jared Gilleland <ejgilleland@gmail.com>
2021-03-18 22:30:49 -0700

    Moved callback functions for keydown event listener to car object as methods. Created a coin object from the static coin element in html and added collision checking logic to the coin object. #6

--- main.js
+++ main.js
@@ -5,42 +5,64 @@ var car = {
   moving: false,
   movingId: null,
   positionX: 0,
-  positionY: 0
+  width: 204,
+  positionY: 0,
+  height: 204,
+  driving: function() {
+    if (car.direction === 'east') {
+      car.positionX += 8;
+      car.$car.style.left = car.positionX + 'px';
+    } else if (car.direction === 'south') {
+      car.positionY += 8;
+      car.$car.style.top = car.positionY + 'px';
+    } else if (car.direction === 'west') {
+      car.positionX -= 8;
+      car.$car.style.left = car.positionX + 'px';
+    } else if (car.direction === 'north') {
+      car.positionY -= 8;
+      car.$car.style.top = car.positionY + 'px';
+    }
+  },
+  steering: function(event) {
+    if (event.key === ' ') {
+      car.moving = !car.moving;
+      if (car.moving) {
+        car.movingId = window.setInterval(car.driving, 16);
+      } else {
+        window.clearInterval(car.movingId);
+      }
+    } else if (event.key === 'ArrowRight') {
+      car.direction = 'east';
+      car.$car.className = car.direction;
+    } else if (event.key === 'ArrowDown') {
+      car.direction = 'south';
+      car.$car.className = car.direction;
+    } else if (event.key === 'ArrowLeft') {
+      car.direction = 'west';
+      car.$car.className = car.direction;
+    } else if (event.key === 'ArrowUp') {
+      car.direction = 'north';
+      car.$car.className = car.direction;
+    }
+  }
 };
 
-window.addEventListener('keydown', function (event) {
-  if (event.key === ' ') {
-    car.moving = !car.moving;
-    if (car.moving) {
-      car.movingId = window.setInterval(function () {
-        if (car.direction === 'east') {
-          car.positionX += 8;
-          car.$car.style.left = car.positionX + 'px';
-        } else if (car.direction === 'south') {
-          car.positionY += 8;
-          car.$car.style.top = car.positionY + 'px';
-        } else if (car.direction === 'west') {
-          car.positionX -= 8;
-          car.$car.style.left = car.positionX + 'px';
-        } else if (car.direction === 'north') {
-          car.positionY -= 8;
-          car.$car.style.top = car.positionY + 'px';
-        }
-      }, 16);
-    } else {
-      window.clearInterval(car.movingId);
+var coin = {
+  $coin: document.querySelector('.coin'),
+  positionX: 500,
+  width: 35,
+  positionY: 500,
+  height: 35,
+  collisionCheck: function() {
+    if (car.positionX < coin.positionX + coin.width &&
+      car.positionX + car.width > coin.positionX &&
+      car.positionY < coin.positionY + coin.height &&
+      car.positionY + car.height > coin.positionY) {
+      console.log('poots');
     }
-  } else if (event.key === 'ArrowRight') {
-    car.direction = 'east';
-    car.$car.className = car.direction;
-  } else if (event.key === 'ArrowDown') {
-    car.direction = 'south';
-    car.$car.className = car.direction;
-  } else if (event.key === 'ArrowLeft') {
-    car.direction = 'west';
-    car.$car.className = car.direction;
-  } else if (event.key === 'ArrowUp') {
-    car.direction = 'north';
-    car.$car.className = car.direction;
   }
-});
+};
+
+window.addEventListener('keydown', car.steering);
+
+setInterval(coin.collisionCheck, 16);
